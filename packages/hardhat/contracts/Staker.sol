3f9e3fca7a4e83ebfd511bb37bca9675a2e50f52
yehorrudytsia <yehorrudytsia@gmail.com>
2022-01-30 08:26:02 +0200

    Implement staker contract body

--- packages/hardhat/contracts/Staker.sol
+++ packages/hardhat/contracts/Staker.sol
@@ -3,7 +3,11 @@ pragma solidity 0.8.4;
 import "hardhat/console.sol";
 import "./ExampleExternalContract.sol";
 
+
 contract Staker {
+
+  mapping (address => uint256) public balances;
+  uint256 public threshold = 5 ether;
 
   ExampleExternalContract public exampleExternalContract;
 
@@ -11,10 +15,24 @@ contract Staker {
       exampleExternalContract = ExampleExternalContract(exampleExternalContractAddress);
   }
 
+  function stake() external payable {
+
+    balances[msg.sender] += msg.value;
+  }
   // Collect funds in a payable `stake()` function and track individual `balances` with a mapping:
   //  ( make sure to add a `Stake(address,uint256)` event and emit it for the frontend <List/> display )
 
+  function execute() external {
+    if (address(this).balance >= threshold)
+      exampleExternalContract.complete{value: address(this).balance}();
+  }
 
+  function withdraw() external {
+    uint256 amount = balances[msg.sender];
+    balances[msg.sender] -= amount;
+    (bool success, ) = (msg.sender).call{value: amount}("");
+    require(success, "Failed to withdraw Ether");
+  }
   // After some `deadline` allow anyone to call an `execute()` function
   //  It should either call `exampleExternalContract.complete{value: address(this).balance}()` to send all the value
 
@@ -32,3 +50,4 @@ contract Staker {
 
 
 }
+
